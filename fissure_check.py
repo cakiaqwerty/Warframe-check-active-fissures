96c5a126a19632a9c1de6b890818446ba221579e
Cakir <muhdsyakira@gmail.com>
2026-04-07 21:06:15 +0800

    changed print to return in check_fissure

--- fissure_check.py
+++ fissure_check.py
@@ -14,12 +14,14 @@ def check_fissure(SolNode, SP_Mode=True):
     for fissure in fissures:
         if SP_Mode:
             if fissure.get("Node")==SolNode and fissure.get("Hard"): #SP Only
-                print (f"{SolNode} Steel Path Active")
+                status = (f"{SolNode} Steel Path Active")
                 time_left(SolNode, fissure)
+                return (status, time_left)
         else:
             if fissure.get("Node")==SolNode: # SP+Normal Path
-                print (f"{SolNode} Active")
+                status = (f"{SolNode} Active")
                 time_left(SolNode, fissure)
+                return (status, time_left)
 
         # print (mission.get("Node"), mission.get("MissionType"), mission.get("Hard"))
         # if fissure.get("Node") == "SolNode310":
@@ -44,9 +46,9 @@ def time_left(SolNode, fissure):
         minutes, seconds = divmod(remainder, 60)
         print(f"{SolNode} | Time Remaining: {hours}h {minutes}m {seconds}s")
 
-Cascade = "SolNode232"
-if not check_fissure(Cascade):
-    print(f"No active SP Cascade")
+# Cascade = "SolNode232"
+# if not check_fissure(Cascade):
+#     print(f"No active SP Cascade")
 
 # LithCapture = "SolNode401"
 # check_fissure(LithCapture, False)
@@ -61,16 +63,22 @@ if not check_fissure(Cascade):
 # check_fissure(MesoNeoExterminate, False)
 
 mission_list = [
-    {"mission_type": "Cascade", "node_num": "SolNode232"},
-    {"mission_type": "LithCapture", "node_num": "SolNode401"},
-    {"mission_type": "LithExterminate", "node_num": "SolNode400"},
-    {"mission_type": "MesoNeoCapture", "node_num": "SolNode406"},
-    {"mission_type": "MesoNeoExterminate", "node_num": "SolNode407"},
+    {"mission_type": "Void Cascade", "node_num": "SolNode232"},
+    {"mission_type": "Lith -- Capture", "node_num": "SolNode401"},
+    {"mission_type": "Lith -- Exterminate", "node_num": "SolNode400"},
+    {"mission_type": "Meso/Neo -- Capture", "node_num": "SolNode406"},
+    {"mission_type": "Meso/Neo -- Exterminate", "node_num": "SolNode407"},
+    {"mission_type": "Neo/Axi -- Void Survival", "node_num": "SolNode409"},
+    {"mission_type": "Meso/Neo/Axi -- Void Defense", "node_num": "SolNode408"}
 ]
 
-for mission in mission_list[1:]:
-    if not check_fissure(mission["node_num"], False):
-        print(f"No active {mission["mission_type"]}")
+for mission in mission_list:
+    if check_fissure(mission["node_num"], True) is None:
+        print (f"No active {mission["mission_type"]}")
+
+# for mission in mission_list[1:]:
+#     if not check_fissure(mission["node_num"], False):
+#         print(f"No active {mission["mission_type"]}")
 
 #Mission nodes: https://wiki.warframe.com/w/World_State#Node
 #Cascade = SolNode232
