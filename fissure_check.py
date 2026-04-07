e1a900656b9addd26c10db1d4d10e8ccd5cb92f7
Cakir <muhdsyakira@gmail.com>
2026-04-08 01:00:41 +0800

    Improved check_fissure, toggle SP only TODO: Update README

--- fissure_check.py
+++ fissure_check.py
@@ -9,20 +9,53 @@ worldstate = response.json()
 # Get active fissures
 fissures = worldstate.get("ActiveMissions", [])
 
+modifier_map = {
+    "VoidT1": "Lith",
+    "VoidT2": "Meso",
+    "VoidT3": "Neo",
+    "VoidT4": "Axi",
+    "VoidT5": "Requiem",
+    "VoidT6": "Omnia"
+}
+
+# Init
+mission_list = [
+    {"mission_type": "Void Cascade", "node_num": "SolNode232"},
+    {"mission_type": "Lith Capture", "node_num": "SolNode401"},
+    {"mission_type": "Lith Exterminate", "node_num": "SolNode400"},
+    {"mission_type": "Meso/Neo Capture", "node_num": "SolNode406"},
+    {"mission_type": "Meso/Neo Exterminate", "node_num": "SolNode407"},
+    {"mission_type": "Neo/Axi Void Survival", "node_num": "SolNode409"},
+    {"mission_type": "Meso/Neo/Axi Void Defense", "node_num": "SolNode408"}
+]
+
+sp_only = False
+
+# Functions
+
 def check_fissure(mission, SP_Mode=True):
 
     for fissure in fissures:
+
         if SP_Mode:
-            if fissure.get("Node")==mission["node_num"] and fissure.get("Hard"): #SP Only
-                status = (f"{mission["mission_type"]} Steel Path Active")
+        #SP Only
+            if fissure.get("Node")==mission["node_num"] and fissure.get("Hard"):
+                mission_status = (mission["mission_type"])
                 time_left = check_time_left(mission["node_num"], fissure)
-                return (status, time_left)
+                relic_tier = fissure.get("Modifier")
+                sp_status = fissure.get("Hard")
+                return (mission_status, time_left, relic_tier, sp_status)
+        
         else:
-            if fissure.get("Node")==mission["node_num"]: # SP+Normal Path
-                status = (f"{mission["mission_type"]} Active")
+        #SP + Normal Path
+            if fissure.get("Node")==mission["node_num"]:
+                mission_status = (mission["mission_type"])
                 time_left = check_time_left(mission["node_num"], fissure)
-                return (status, time_left)
-    return mission["mission_type"], None
+                relic_tier = fissure.get("Modifier")
+                sp_status = fissure.get("Hard")
+                return (mission_status, time_left, relic_tier, sp_status)
+            
+    return mission["mission_type"], None, None, None
 
         # print (mission.get("Node"), mission.get("MissionType"), mission.get("Hard"))
         # if fissure.get("Node") == "SolNode310":
@@ -47,22 +80,17 @@ def check_time_left(SolNode, fissure):
         minutes, seconds = divmod(remainder, 60)
         return (f"Time Remaining: {hours}h {minutes}m {seconds}s")
 
-mission_list = [
-    {"mission_type": "Void Cascade", "node_num": "SolNode232"},
-    {"mission_type": "Lith -- Capture", "node_num": "SolNode401"},
-    {"mission_type": "Lith -- Exterminate", "node_num": "SolNode400"},
-    {"mission_type": "Meso/Neo -- Capture", "node_num": "SolNode406"},
-    {"mission_type": "Meso/Neo -- Exterminate", "node_num": "SolNode407"},
-    {"mission_type": "Neo/Axi -- Void Survival", "node_num": "SolNode409"},
-    {"mission_type": "Meso/Neo/Axi -- Void Defense", "node_num": "SolNode408"}
-]
-
+# Main START
+if sp_only:
+    print ("================== SP ONLY ==================")
+else:
+    print ("================== NORMAL + SP ==================")
 for mission in mission_list:
-    mission_type, time_left = check_fissure(mission, False) #True = SP Only #False = SP + Normal Path
+    mission_type, time_left, relic_tier, sp_status = check_fissure(mission, sp_only)
     if time_left is None:
         print (f"No active {mission_type}")
     else:
-        print(f"{mission_type} | {time_left}")
+        print(f"{mission_type} | {time_left} | {modifier_map.get(relic_tier)} | SP: {sp_status}")
 
 #Mission nodes: https://wiki.warframe.com/w/World_State#Node
 #Cascade = SolNode232
